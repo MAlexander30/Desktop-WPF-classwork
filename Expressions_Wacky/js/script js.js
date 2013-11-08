58face54d876627306712ca79900ff6e5fa5485f
MAlexander30 <willquita@yahoo.com>
2013-11-08 11:09:54 -0600

    week 2 experssion

--- Expressions_Wacky/js/script js.js
+++ Expressions_Wacky/js/script js.js
@@ -1,9 +1,15 @@
 /**
  * Created by marquitaalexander on 11/8/13.
  */
-//how many crayons will each ki
+
+//how many crayons within each crayon box will each kid recieve
 var crayonBox = 24;
 var Kids = 80;
 var crayon = 8
-var cpc = 24 * 8/80
+//multiply how many crayons total and divided the crayons to each kid
+var cpc = crayonBox * crayon/Kids
+//results show many crayons each child receives
 console.log(cpc)
+
+
+var 
