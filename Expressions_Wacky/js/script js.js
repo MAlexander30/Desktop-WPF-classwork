223a3e8f1bbc60a38124c5127d074e22bb112e94
MAlexander30 <willquita@yahoo.com>
2013-11-08 10:59:16 -0600

    week 2 experssion

--- Expressions_Wacky/js/script js.js
+++ Expressions_Wacky/js/script js.js
@@ -1,5 +1,9 @@
 /**
  * Created by marquitaalexander on 11/8/13.
  */
-
-alert("test");
+//how many crayons will each ki
+var crayonBox = 24;
+var Kids = 80;
+var crayon = 8
+var cpc = 24 * 8/80
+console.log(cpc)
