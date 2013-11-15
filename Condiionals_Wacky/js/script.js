66728a75a3501531f3ba739bc355cd33839dbf1b
MAlexander30 <willquita@yahoo.com>
2013-11-15 15:24:19 -0600

    Week three assignment

---
+++ Condiionals_Wacky/js/script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by marquitaalexander on 11/15/13.
+ */
+
+//if statement
+var fishBowels = 13;
+//if there are enough fish bowel for the event then it will print "Time to decorate for the party"
+//if there is over 13 guest
+if(fishBowels < 50){
+    //its true if there is more guest then fishBowels
+    console.log("Time to decorate for the party");
+}
+
+//else if
+var fishBowels = 13
+var Fish = 23
+var fryFish = 10 //how many fryers to cook extra fish
+//if there are more fish then fishBowels then it will read "Where is the seasoning"
+if(Fish > fishBowels){
+    console.log("Where is the seasoning");
+}else if(Fish < fryFish){
+    //if there is more Fish then the fryer
+    console.log("Swim on swim on");
+}else{
+    console.log("Just let them go free");
+    //if none of the equations work then let the fish free
+}
+
+//else block
+var fishBowels = 20
+var Fish = 13
+//if there is less fish then fishBowels then it will print "Swim on swim on"
+if(Fish > fishBowels){
+    //its only true then the fish or safe
+    console.log("Swim on swim on");
+}else{
+    //if false then they will fry
+    console.log("Time to eat");
+}
+
+
